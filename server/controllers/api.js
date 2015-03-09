a636d68ba402cd4157c92102e7c1b52981980244
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 12:28:35 -1000

    restful api created

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,8 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require('../models/todo');
 
+// list all todos
 router.get('/', function (req, res) {
-  res.send("HELLO API");
+  Todo.find(function (err, todos){
+    if (err) throw err;
+    res.json( todos );
+  });
+});
+
+// add todo 
+router.post('/', function (req, res) {
+  // create with singular todo
+  Todo.create({ title : req.body.title }, function (err, todo) {
+    if (err) throw err;
+    res.json( todo );
+  });
+});
+
+// delete todo
+router.delete('/:id', function (req, res) {
+  Todo.remove({_id : req.params.id }, function (err, todo) {
+    if (err) throw err;
+    res.json( todo ); // this is the todo that was removed
+  });
+});
+
+// complete todo
+router.put('/:id/complete', function (req, res) {
+  Todo.update({ _id : req.params.id },
+   {
+    $set : {
+      completed : true
+    }
+   }, function ( err, todo) {
+    if (err) throw err;
+    res.json( todo );  // this is the todo that was updated
+   });
+});
+
+// uncomplete todo
+router.put('/:id/uncomplete', function (req, res) {
+  Todo.update({ _id : req.params.id },
+   {
+    $set : {
+      completed : false
+    }
+   }, function ( err, todo) {
+    if (err) throw err;
+    res.json( todo );  // this is the todo that was updated
+   });
 });
 
 module.exports = router;
