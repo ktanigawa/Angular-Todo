61c0b7b1315870f35e03f66c9102533f22cc0c0c
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 10:36:17 -1000

    dependencies app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+angular
+  // dependencies----v[]
+  .module('TodoApp', [])
+
+    // dependencies------------------v
+  .controller('TodoController', ['scope', function($scope){
+    
+    $scope.name = "Is Hooked Up";
+
+  }]);
+
