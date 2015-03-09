a50ec5a8d08c30d68415f43007a618d40a98123f
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 10:59:32 -1000

    todo item array

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 angular
-  // dependencies----v[]
+
   .module('TodoApp', [])
-
-    // dependencies------------------v
-  .controller('TodoController', ['scope', function($scope){
-    
-    $scope.name = "Is Hooked Up";
-
+  // dependencies-----------------v----------------v
+  .controller('TodoController', ['$scope', function($scope){
+   
+    // one todo called fake title ARRAY of objects
+    $scope.todos = [];
+    $scope.save_todo = function(new_title) {
+      $scope.todos.push({
+        title : new_title,
+        completed : false
+      });
+    };
+    $scope.new_todo = ""; //clear the input
   }]);
 
