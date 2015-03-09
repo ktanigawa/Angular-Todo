bf3e89e086dc8008eb65b9f8e541b72591db8532
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 11:18:27 -1000

    search query filter

--- js/app.js
+++ js/app.js
@@ -6,12 +6,19 @@ angular
    
     // one todo called fake title ARRAY of objects
     $scope.todos = [];
+
     $scope.save_todo = function(new_title) {
       $scope.todos.push({
         title : new_title,
         completed : false
       });
+      $scope.new_todo = ""; //clear the input
     };
-    $scope.new_todo = ""; //clear the input
+
+    $scope.enter_saves = function($event){
+      if( $event.keyCode == 13){//keyCode for [enter key]
+        $scope.save_todo( $scope.new_todo );
+      }
+    };
   }]);
 
