158afc402e47bfbe9ef51b6324ef439dce8a852a
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 15:10:51 -1000

    saving titles to the server

--- public/js/app.js
+++ public/js/app.js
@@ -16,6 +16,9 @@ angular
         completed : false
       });
       $scope.new_todo = ""; //clear the input
+
+      // save to db
+      TodoService.create({ title : new_title });
     };
 
     $scope.enter_saves = function($event){
--- public/js/todoservice.js
+++ public/js/todoservice.js
@@ -4,9 +4,13 @@ angular // calling framework
 
   // dependency injection---v--$----------------v
   .service('TodoService', ['$http', function($http){
-
+    // GET /api returns array of all todos
     this.list = function () {
       return $http.get('/api'); // return promise
     };
 
+    // POST /api creates a new todo
+    this.create = function ( todo ) {
+      return $http.post('/api', todo ); // returns promise
+    };
   }]);
