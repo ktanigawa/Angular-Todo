143eadc141e572da171b6cbceddd6a3bead7d1e6
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-09 15:56:02 -1000

    fixes undefined _id on the new todos

--- public/js/app.js
+++ public/js/app.js
@@ -11,18 +11,22 @@ angular
     // $scope.todos = TodoService.list(); //promise
 
     $scope.save_todo = function(new_title) {
-      $scope.todos.push({
+      var new_todo = {
+        _id : "unassigned",
         title : new_title,
         completed : false
-      });
+      };
+      $scope.todos.push(new_todo);
       $scope.new_todo = ""; //clear the input
 
       // save to db
-      TodoService.create({ title : new_title });
+      TodoService.create({ title : new_title }).then(function (response){
+        new_todo._id = response.data._id;
+      });
     };
 
     $scope.enter_saves = function($event){
-      if( $event.keyCode == 13){//keyCode for [enter key]
+      if( $event.keyCode == 13){ //keyCode for [enter key]
         $scope.save_todo( $scope.new_todo );
       }
     };
